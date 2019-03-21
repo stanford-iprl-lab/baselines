9247de045f7dd0b9cfe1b0796449b4ed53220c38
Krishnan <krshna@stanford.edu>
2019-03-21 07:40:07 +0000

    updated run.sh, analyze-seeds notebook, all lr experiments uploaded

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
-for SEED in 1 100 200
-do 
-    for LR in .001
-    do
-        echo "starting training, LR=${LR} SEED=${SEED}" 
-        python baselines/run_ppo.py --lr ${LR} --seed $SEED --exp-name lr-$LR-seed-$SEED --env ReacherPyBulletEnv-v0 --network mlp --num_env 12  --num_timesteps 2e7 &> ../outs/LR-$LR-SEED-$SEED.out &
-        sleep 10
-    done
-done
+LR=.00025
+CR=.2
+NMB=128
+NOPT=10
+SEED=1
+echo "starting training, LR=$LR SEED=${SEED}"
+python baselines/run_ppo.py --lr $LR --seed $SEED --exp-name hopper-lr-$LR-cr-$CR-crflip-nmb-$NMB-nopt-$NOPT-seed-$SEED --env HopperBulletEnv-v0 \
+      --network mlp --num_env 12 --nminibatches $NMB --noptepochs $NOPT --cliprange_anneal --cliprange_flip --cliprange $CR --num_timesteps 2e7 &> ../outs/HOPPER-LR-$LR-CR-$CR-CRFLIP-NMB-$NMB-NOPT-$NOPT-SEED-${SEED}.out &
